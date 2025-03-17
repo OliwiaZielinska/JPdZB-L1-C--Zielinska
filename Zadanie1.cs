2ca93d203c74f67286224ee4f6e8962a0a5a7218
Oliwia Zielińska <268480@student.pwr.edu.pl>
2025-03-17 20:33:25 +0100

    Creating a repository and adding task 1

---
+++ Zadanie1.cs
@@ -0,0 +1,62 @@
+﻿/* Author: Oliwia Zielińska
+Linki, z których korzystano podczas realizacji zadania:
+WZÓR HARONA: https://pl.wikipedia.org/wiki/Wz%C3%B3r_Herona
+TWORZENIE KLAS W C#: https://dev-hobby.pl/csharp/funkcje-metody-w-c/ oraz https://www.plukasiewicz.net/CSharp_dla_poczatkujacych/Klasy
+*/
+// ZADANIE NR 1
+using System;
+
+public class Heron
+{   // zdefiniowanie pól klasy
+    public int a;
+    public int b;
+    public int c;
+
+    // metoda do przetestowania działania funkcji herona
+    public static void Main(string[] args)
+    {
+        Heron heronObiekt = new Heron();
+
+        // Test poprawnego działania
+        TestHerona(heronObiekt, 3, 4, 5);
+
+        // Test poprawnego działania
+        TestHerona(heronObiekt, 7, 5, 10);
+
+        // Test dla ujemnego boku
+        TestHerona(heronObiekt, -3, 4, 2);
+
+        // Test dla boku o długości 0
+        TestHerona(heronObiekt, 3, 4, 0);
+
+        // Test dla boków, które nie utworzą trójkąta
+        TestHerona(heronObiekt, 1, 1, 2);
+    }
+
+    public static void TestHerona(Heron heronObiekt, int a, int b, int c)
+    {
+        try
+        {
+            double S = heronObiekt.heron(a, b, c);
+            Console.WriteLine($"Pole S dla trójkąta o bokach {a}, {b}, {c} wynosi: {S}");
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine($"Błąd dla boków {a}, {b}, {c}: {e.Message}");
+        }
+    }
+
+
+    // funkcja do wyliczania pola trójkąta S wykorzystująca wzór Herona
+    public double heron(int a, int b, int c){
+    if (a <= 0 || b <= 0 || c <= 0){
+        throw new ArgumentException("Boki trójkąta nie mogą być liczbami ujemnymi lub równymi 0!");
+    }
+    double obwod = (0.5)*(a+b+c);
+    double S = Math.Sqrt(obwod*(obwod-a)*(obwod-b)*(obwod-c));
+    if (S <= 0){
+        throw new ArgumentException("Z podanych długości boków nie można utworzyć trójkąta!");
+    }
+    return S;
+    }
+}
